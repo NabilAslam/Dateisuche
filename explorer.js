88da48834422b17ece1bac948da1a0b106547467
Nabil Aslam <aslam@versacommerce.de>
2023-10-05 16:20:28 +0200

    done

---
+++ explorer.js
@@ -0,0 +1,19 @@
+// Get the file server to read, create, update, delete and rename files
+// Get the path to find files
+const fs = require("fs");
+const path = require("path")
+
+// Function to retrieve files with certain extensions in folder
+function folder() {
+    let datas = fs.readdirSync(path.resolve(__dirname, "public"))
+
+    let datafiles = datas.filter(data => data.includes('.html'));
+
+    datas.forEach(res => {
+        fs.lstatSync(path.resolve(__dirname,"public", res)).isDirectory()
+    })
+
+    console.log("public:", datafiles)
+}
+
+folder()
